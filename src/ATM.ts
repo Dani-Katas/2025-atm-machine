3f8f417035896be197fdd2a01c3956919878c207
Daniel Ramos <danielramosacosta1@gmail.com>
2025-05-17 09:18:20 +0200

    wip

--- src/ATM.ts
+++ src/ATM.ts
@@ -22,20 +22,12 @@ export class ATM {
 
   withdraw(quantity: number): Array<CountableMoney> {
     if (this.money.length === 2) {
-      if (quantity === 5) {
-        return [
-          ...new ATM([{ denominator: 2, type: "coin" }]).withdraw(4),
-          ...new ATM([{ denominator: 1, type: "coin" }]).withdraw(1),
-        ];
-      }
       if (quantity === 1) {
         return [...new ATM([{ denominator: 1, type: "coin" }]).withdraw(1)];
       }
-
       if (quantity === 2) {
         return [...new ATM([{ denominator: 2, type: "coin" }]).withdraw(2)];
       }
-
       if (quantity === 3) {
         return [
           ...new ATM([{ denominator: 2, type: "coin" }]).withdraw(2),
@@ -45,7 +37,12 @@ export class ATM {
       if (quantity === 4) {
         return [...new ATM([{ denominator: 2, type: "coin" }]).withdraw(4)];
       }
-
+      if (quantity === 5) {
+        return [
+          ...new ATM([{ denominator: 2, type: "coin" }]).withdraw(4),
+          ...new ATM([{ denominator: 1, type: "coin" }]).withdraw(1),
+        ];
+      }
       throw new Error("Unimplemented method ATM#withdraw");
     }
 
