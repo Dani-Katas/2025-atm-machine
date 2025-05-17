9c25fd1975d76ff04250fd3aa319669ef7487c90
Daniel Ramos <danielramosacosta1@gmail.com>
2025-05-17 18:23:13 +0200

    wip

--- src/ATM.ts
+++ src/ATM.ts
@@ -3,7 +3,7 @@ import { type Money, MoneyNew } from "./Money.ts";
 import { Cash } from "./Cash.ts";
 
 export class ATM {
-  private readonly money2: MoneyNew[];
+  private readonly money: MoneyNew[];
 
   public static of(money: Array<Money>): ATM {
     if (!money.length) return new EmptyATM();
@@ -11,15 +11,14 @@ export class ATM {
   }
 
   protected constructor(money: Array<Money>) {
-    this.money2 = money.map((m) => new MoneyNew(m.value, m.type));
+    this.money = money.map((m) => new MoneyNew(m.value, m.type));
   }
 
   withdraw(quantity: number): Array<Denomination> {
-    const [current, ...money] = this.money2;
-    const leftovers = current.leftovers(quantity);
+    const [current, ...money] = this.money;
     return new Cash([
       current.toDenomination(quantity),
-      ...ATM.of(money).withdraw(leftovers),
+      ...ATM.of(money).withdraw(current.leftovers(quantity)),
     ]).toArray();
   }
 }
